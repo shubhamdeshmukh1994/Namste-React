6b3e40051846b4e155f5b769ba50756d01dcb6f6
shubhamdeshmukh1994 <shubhamsolanke84@gmail.com>
2026-05-23 22:22:54 +0530

    accordian with controlled commponent added

--- src/componant/menu/Menu.jsx
+++ src/componant/menu/Menu.jsx
@@ -1,29 +1,49 @@
 import Shimmer from "../shimmar";
 import { useParams } from "react-router-dom";
 import useRestaurantMenu from "../../utils/useRestaurantMenu";
+import Category from "../Category";
+import RestaurantCategory from "./RestaurantCategory";
+import { useState } from "react";
 const RestaurantMenu = () => {
   const restId = useParams();
-  const restInfo = useRestaurantMenu(restId);
-  if (restInfo === null) return <Shimmer />;
+  const restInfo = useRestaurantMenu(restId.resId);
+  const [showIndex, setShowIndex] = useState(0);
+  if (restInfo === null || restInfo.length === 0) {
+    return <Shimmer />;
+  }
 
-  return restInfo.length === 0 ? (
+  const { name, cuisines, costForTwoMessage } =
+    restInfo?.cards[2]?.card?.card?.info || {};
+
+  const itemCards =
+    restInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.[1]?.card
+      ?.card?.itemCards || [];
+
+  const categories =
+    restInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
+      (c) =>
+        c.card?.card?.["@type"] ===
+        "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory",
+    );
+
+  return restInfo?.length === 0 ? (
     <Shimmer />
   ) : (
-    <div>
-      <h1>Restaurant Menu</h1>
-      <ul>
-        {restInfo?.map((item) => (
-          <li key={item.card.info.id}>
-            <h4>{item.card.info.name}</h4>
-            <p>
-              Price: INR:{" "}
-              {item.card.info.defaultPrice
-                ? item.card.info.defaultPrice / 100
-                : item.card.info.price / 100}
-            </p>
-          </li>
-        ))}
-      </ul>
+    <div className="text-center">
+      <h1 className="font-bold my-5 text-2xl">{name}</h1>
+      <p className="font-bold text-lg">
+        {cuisines.join(", ")} - {costForTwoMessage}
+      </p>
+      {categories.map((category, index) => {
+        return (
+          <RestaurantCategory
+            key={category?.card?.card.title}
+            data={category?.card?.card}
+            showItems={index === showIndex ? true : false}
+            setShowIndex={() => setShowIndex(index)}
+          />
+        );
+      })}
     </div>
   );
 };
--- src/utils/useRestaurantMenu.jsx
+++ src/utils/useRestaurantMenu.jsx
@@ -13,15 +13,11 @@ const useRestaurantMenu = (restId) => {
       const response = await fetch(RESTAURANT_MENU_URL + restId);
       if (response.status !== 200) {
         setTimeout(() => {
-          setResMenu(
-            MenuList?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR
-              ?.cards[2].card?.card?.itemCards || [],
-          );
+          setResMenu(MenuList?.data)
         }, 1000);
       } else if (response.status === 200) {
         const jsonData = await response.json();
-        const data = jsonData?.data?.cards[2]?.card?.card?.itemCards || [];
-        setResMenu(data);
+        setResMenu(jsonData?.data);
       }
     } catch (error) {
       console.error("Error fetching menu:", error);
